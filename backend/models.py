73a8b391743474c2487374e9bf81cdfb484fd66e
megankulshekar <91085343+megankulshekar@users.noreply.github.com>
2023-10-14 03:44:33 -0400

    added fields

--- backend/models.py
+++ backend/models.py
@@ -2,10 +2,29 @@ import uuid
 from pydantic import BaseModel, Field
 
 class Book(BaseModel):
-    id: str = Field(default_factory=uuid.uuid4, alias="_id")
+    book_id: str = Field(default_factory=uuid.uuid4, alias="_id")
     title: str = Field(...)
     author: str = Field(...)
     description: str = Field(...)
     genre: str = Field(...)
     cover_img: str = Field(...)
     text_path: str = Field(...)
+
+class User(BaseModel):
+    user_id: str = Field(default_factory=uuid.uuid4, alias="_id")
+    username: str = Field(...)
+    email: str = Field(...)
+    password_hash: str = Field(...)
+
+class Section(BaseModel):
+    section_id: str = Field(default_factory=uuid.uuid4, alias="_id")
+    book_id: str = Field(...)
+    user_id: str = Field(...)
+    start: int = Field(...)
+    end: int = Field(...)
+    img_path: str = Field(...)
+
+class Current(BaseModel):
+    user_id: str = Field(...)
+    book_id: str = Field(...)
+    section_id: str = Field(...)
