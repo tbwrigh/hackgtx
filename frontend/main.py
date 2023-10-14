59c60fa0bc990e215138904bbd9cb99d3e495ded
megankulshekar <91085343+megankulshekar@users.noreply.github.com>
2023-10-14 10:52:01 -0400

    started login.html

--- frontend/main.py
+++ frontend/main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from fastapi.responses import RedirectResponse
 
 app = FastAPI()
@@ -7,7 +9,12 @@ app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+templates = Jinja2Templates(directory="templates")
+
 @app.get("/")
 async def root():
     return RedirectResponse(url="/static/login.html")
 
+@app.get("/")
+async def root():
+    return RedirectResponse(url="/static/home.html")
