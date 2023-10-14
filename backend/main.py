84a8392f8da644a92c3a253fd5a40e8162fcff94
Tyler Wright <tyler.wright5146@gmail.com>
2023-10-14 03:08:35 -0400

    mongo url

--- backend/main.py
+++ backend/main.py
@@ -2,10 +2,18 @@ from fastapi import FastAPI
 
 import dotenv
 import os
+import openai 
+from pymongo import MongoClient
 
 dotenv.load_dotenv()
 
 openai_key = os.getenv("OPENAI_KEY")
+
+openai.api_key = openai_key
+
+mongo_url = os.getenv("MONGO_URL")
+
+client = MongoClient(mongo_url)
 
 app = FastAPI()
 
