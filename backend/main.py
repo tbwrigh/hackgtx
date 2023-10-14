1ff733338ce2e95d3321f28ab7da9aa167a84c34
Tyler Wright <tyler.wright5146@gmail.com>
2023-10-14 03:50:39 -0400

    get section endpoint

--- backend/main.py
+++ backend/main.py
@@ -5,6 +5,8 @@ import os
 import openai 
 from pymongo import MongoClient
 
+from models import User, Book, Section, Current
+
 dotenv.load_dotenv()
 
 openai_key = os.getenv("OPENAI_KEY")
@@ -20,6 +22,10 @@ currentread_db = client['reads']
 book_db = client['books']
 section_db = client['sections']
 
-
 app = FastAPI()
+
+@app.get("/sectiond/{section_id}")
+def get_section(section_id: str) -> Section:
+    section = section_db['sections'].find_one({"section_id": section_id})
+    return section
 
